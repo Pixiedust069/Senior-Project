80caf9778189c81e230c4cd8a3973d374a7025e6
digikingj <juliusfrancis116@hotmail.com>
2015-02-26 00:26:23 -0400

    Menu Touches

---
+++ Assets/Scripts/Load_Characters_Scene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Load_Characters_Scene : MonoBehaviour {
+	
+	public void LoadScene (int level)
+	{
+		Application.LoadLevel (level);
+	}
+}
---
+++ Assets/Scripts/Load_How_To_Play.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Load_How_To_Play : MonoBehaviour {
+
+	public void LoadScene (int level)
+	{
+		Application.LoadLevel (level);
+		}
+}
